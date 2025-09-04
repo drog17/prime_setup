72afb28b253e2d50946a309ced98c768c9dbd201
Nailbro <naillyalyaza55@gmail.com>
2025-09-04 13:57:03 +0600

    Faq

--- src/app/Faq/sections/Poisk/Poisk.tsx
+++ src/app/Faq/sections/Poisk/Poisk.tsx
@@ -1,15 +1,34 @@
-import Image from 'next/image'
-import styles from './Poisk.module.scss'
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./Poisk.module.scss";
+import Biznes from "../Biznes/Biznes"; 
+
 export default function Poisk() {
-    return(
-        <section className={styles.section}>
-            <div className={styles.contend}>
-               <h1>Ищете конкретный вопрос?</h1>
-               <form action="">
-                <input type="search" name="" id="" placeholder='Введите чтобы начать поиск...'/>
-               <Image src={"/icons/Component 1 (9).svg"} alt='Component' width={30} height={30}/><button>Поиск</button>
-                </form> 
-            </div>
-        </section>
-    )
+  const [query, setQuery] = useState("");
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.contend}>
+        <h1>Ищете конкретный вопрос?</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="search"
+            placeholder="Введите чтобы начать поиск..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            required
+          />
+          <Image
+            src={"/icons/Component 1 (9).svg"}
+            alt="Component"
+            width={30}
+            height={30}
+          />
+          <button>Поиск</button>
+        </form>
+      </div>
+      <Biznes searchQuery={query} />
+    </section>
+  );
 }
