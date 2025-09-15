82a1dfbaa6a62088228bfdb2b73dcf93aa820acf
BH <drog.the.same@gmail>
2025-09-15 11:24:59 +0600

    test commit

--- src/app/Faq/page.tsx
+++ src/app/Faq/page.tsx
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
 import Herod from "./Herod/Herod";
 import Poisk from "./sections/Poisk/Poisk";
 import Header from "@/widget/header/Header";
 import Contacts from "../(home)/sections/Contacts/Contacts";
 import Footer from "@/widget/footer/Footer";
-=======
-import Poisk from "./sections/Poisk/Poisk";
-import Header from "@/widget/header/Header";
-import Footer from "@/widget/footer/Footer";
-import Herod from "./Herod/Herod";
-import Contacts from "../(home)/sections/Contacts/Contacts";
->>>>>>> e24245d3c52d90ba9f3afba4cdbcbd78adc3b935
 
 export default function Faq() {
     return (
         <>
-<<<<<<< HEAD
             <Header />
             <Herod />
             <Poisk />
             <Contacts />
-            <Footer/>
-=======
-<Header/>
-<Herod/>
-<Poisk/>
-<Contacts/>
-<Footer/>
->>>>>>> e24245d3c52d90ba9f3afba4cdbcbd78adc3b935
+            <Footer />
         </>
     )
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import Heros from "../Onas/Heros/Heros";
-import Missiya from "../Onas/sections/Missiya/Missiya";
-import Predprimial from "../Onas/sections/Predprinial/Predprimial";
-import Prime from "../Onas/sections/Prime/Prime";
-import Commanda from "../Onas/sections/Comanda/Comanda";
-import Header from "@/widget/header/Header";
-import Footer from "@/widget/footer/Footer";
-=======
-
 import Missiya from "./Onas/sections/Missiya/Missiya";
 import Predprimial from "./Onas/sections/Predprinial/Predprimial";
 import Prime from "./Onas/sections/Prime/Prime";
@@ -15,7 +5,6 @@ import Commanda from "./Onas/sections/Comanda/Comanda";
 import Header from "@/widget/header/Header";
 import Footer from "@/widget/footer/Footer";
 import Heros from "./Onas/Heros/Heros";
->>>>>>> e24245d3c52d90ba9f3afba4cdbcbd78adc3b935
 export default function About() {
     return (
         <>
@@ -25,11 +14,7 @@ export default function About() {
             <Missiya />
             <Predprimial />
             <Commanda />
-<<<<<<< HEAD
-            <Footer />
-=======
             <Footer/>
->>>>>>> e24245d3c52d90ba9f3afba4cdbcbd78adc3b935
         </>
     )
 }
